7bd4e2a27ef4cd5de9b503c0aa1dbe34543786f0
Kamaleshwaran07 <pskamaleshwaran@gmail.com>
2024-03-29 14:23:28 +0530

    one to updated

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -70,7 +70,7 @@ const Login = ({baseURL}) => {
                         <div>{responseMsg}</div>
                    <Link to={'/forgotpassword'} className='font-semibold ms-auto text-white mt-4'>Forgot Password?</Link>
                     <button type='submit' className='mt-2 font-bold text-xl rounded-lg ms-auto me-auto bg-white shadow-xl w-36 p-2'>Login</button>
-                    <p className='mt-3 ms-auto text-white font-semibold'>Don't have an account? <Link href={'/signup'} className='text-[#1E1E24] underline text-lg'>Sign up here</Link> </p>
+                    <p className='mt-3 ms-auto text-white font-semibold'>Don't have an account? <Link to={'/signup'} className='text-[#1E1E24] underline text-lg'>Sign up here</Link> </p>
 
                 </div>
                 <div className='h-4/6 p-6 mt-28'>
