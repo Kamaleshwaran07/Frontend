9b1717265a54869c9bf4604fce4a03da17af102f
Kamaleshwaran07 <pskamaleshwaran@gmail.com>
2024-03-31 11:23:41 +0530

    resetpassword

--- src/Components/Resetpassword.jsx
+++ src/Components/Resetpassword.jsx
@@ -1,4 +1,4 @@
-import React, {useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios'
 import { Link } from 'react-router-dom';
 
@@ -14,14 +14,15 @@ const Resetpassword = ({ baseURL }) => {
         e.preventDefault()
         const payload = { username }
         // console.log(payload);
-// Sending the username as payload to get the data about user
+        // Sending the username as payload to get the data about user
         try {
             const res = await axios.post(`${baseURL}getUserInfo/`, payload);
-            setResponseMsg(res.data.message);
-            const {user} = res.data; // Extract userId and token from response
+            const  {user}  = res.data;
+            // console.log(user);// Extract userId and token from response
+            // setResponseMsg(user.message);
             setUserId(user._id); // Set userId in state
             setToken(user.token); // Set token in state
-            
+
         }
         catch (error) {
 
@@ -29,7 +30,7 @@ const Resetpassword = ({ baseURL }) => {
         }
 
     };
-// Handle Resetting the password
+    // Handle Resetting the password
     const handleSubmit = async (e) => {
         e.preventDefault();
         const payload = { password };
@@ -44,15 +45,15 @@ const Resetpassword = ({ baseURL }) => {
     return (
         <div className='fixed left-1/3 top-1/3 me-auto p-2 drop-shadow-2xl w-2/5 border-2 rounded-lg h-auto   border-solid border-slate-500 bg-green-500'>
             <div className='text-4xl text-center'>
-            Reset Password
+                Reset Password
             </div>
             <form onSubmit={handleSubmit} >
                 <label className='ms-3 font-semibold text-lg' name='username'>
                     Username:
                 </label>
-                    <br />
-                    <input className='h-10 font-normal ms-2  w-3/5 pr-12 pl-3 py-2 border-2 border-solid border-blue-100 shadow-xl rounded-md' type='email' name='username' placeholder='Enter  your registered mail id' value={username} onChange={(e) => setUsername(e.target.value)} />
-                    <button className='mt-2 mb-2 font-bold text-md rounded-lg ms-6 bg-white shadow-xl p-2' type='button' onClick={getUserId} >Check the username</button>
+                <br />
+                <input className='h-10 font-normal ms-2  w-3/5 pr-12 pl-3 py-2 border-2 border-solid border-blue-100 shadow-xl rounded-md' type='email' name='username' placeholder='Enter  your registered mail id' value={username} onChange={(e) => setUsername(e.target.value)} />
+                <button className='mt-2 mb-2 font-bold text-md rounded-lg ms-6 bg-white shadow-xl p-2' type='button' onClick={getUserId} >Check the username</button>
                 <br />
                 <label name='password' className='ms-3 font-semibold text-lg'>
                     New Password:
@@ -61,11 +62,11 @@ const Resetpassword = ({ baseURL }) => {
                         value={password} onChange={(e) => setPassword(e.target.value)} />
                 </label>
                 <br />
-                    <button className='mt-6 mb-2 font-bold text-md rounded-lg ms-12 bg-white shadow-xl p-2' type='submit' >Reset Password</button>
+                <button className='mt-6 mb-2 font-bold text-md rounded-lg ms-12 bg-white shadow-xl p-2' type='submit' >Reset Password</button>
                 <Link className='pt-2 ms-6 text-lg font-semibold text-white decoration-sky-900 underline underline-offset-4' to='/login'>Click here to go to Login page</Link>
                 <div className='mb-2 mt-4 font-semibold ms-6 text-lg bg-white rounded-lg px-4 w-80'>{responseMsg}</div>
             </form>
-            
+
         </div>
 
     );
